dadb75524c98dee156902e2b0d35cb1178d87bd2
Jeffrey Agudelo <jeffaporta@gmail.com>
2022-03-30 23:16:52 -0500

    x

--- 00Libs/matematicas/rangos.js
+++ 00Libs/matematicas/rangos.js
@@ -11,46 +11,151 @@ AJUSTE_REFLEJO_INVERSO = 5;
 
 class Rango {
   constructor(extremo1, extremo2) {
-    if (!(extremo1 instanceof Number)) {
+    if (typeof extremo1 != "number") {
       throw "el extremo1 no puede ser: " + extremo1;
     }
     if (extremo2 == undefined) {
-      this.extremoInicial = 0;
-      this.extremoFinal = extremo1;
+      this.lowerLimit = 0;
+      this.upperLimit = extremo1;
     } else {
-      if (!(extremo2 instanceof Number)) {
+      if (typeof extremo2 != "number") {
         throw "el extremo2 no puede ser: " + extremo2;
       }
-      this.extremoInicial = extremo1;
-      this.extremoFinal = extremo2;
+      this.lowerLimit = extremo1;
+      this.upperLimit = extremo2;
     }
   }
 
-  ajustar(extremo1, extremo2) {
-    if (!(extremo1 instanceof Number)) {
+  set(extremo1, extremo2) {
+    if (typeof extremo1 != "number") {
       throw "el extremo1 no puede ser: " + extremo1;
     }
     if (extremo2 == undefined) {
-      this.extremoInicial = 0;
-      this.extremoFinal = extremo1;
+      this.lowerLimit = 0;
+      this.upperLimit = extremo1;
     } else {
-      if (!(extremo2 instanceof Number)) {
+      if (typeof extremo2 != "number") {
         throw "el extremo2 no puede ser: " + extremo2;
       }
-      this.extremoInicial = extremo1;
-      this.extremoFinal = extremo2;
+      this.lowerLimit = extremo1;
+      this.upperLimit = extremo2;
     }
+  }
+
+  get head() {
+    return Math.max(this.upperLimit, this.lowerLimit);
+  }
+
+  get tail() {
+    return Math.min(this.upperLimit, this.lowerLimit);
   }
 
   get mag() {
-    return this.extremoFinal - this.extremoInicial;
+    return this.upperLimit - this.lowerLimit;
   }
 
-  get sentido(){
-     return Math.sign(this.mag)
+  get sense() {
+    return Math.sign(this.mag);
   }
 
   invertirSentido() {
-    this.ajustar(this.extremoFinal, this.extremoInicial);
+    this.set(this.upperLimit, this.lowerLimit);
+  }
+
+  valoration(n, incluyente = true) {
+    if (n == this.upperLimit && incluyente) {
+      return POR_DENTRO;
+    }
+    if (this.lowerLimit < this.upperLimit) {
+      if (n < this.lowerLimit) {
+        return POR_DETRAS;
+      }
+      if (n > this.upperLimit) {
+        return POR_DELANTE;
+      }
+      return POR_DENTRO;
+    } else {
+      if (n > this.lowerLimit) {
+        return POR_DETRAS;
+      }
+      if (n <= this.upperLimit) {
+        return POR_DELANTE;
+      }
+      return POR_DENTRO;
+    }
+  }
+
+  lerp(t) {
+    if (this.lowerLimit == 0) {
+      return this.upperLimit * t;
+    }
+    return (this.upperLimit - this.lowerLimit) * t + this.lowerLimit;
+  }
+
+  constrain(t) {
+    if (t > this.head) {
+      return this.head;
+    }
+    if (t < this.tail) {
+      return this.tail;
+    }
+    return t;
+  }
+
+  porcentuality(t) {
+    return (t - this.lowerLimit) / (this.upperLimit - this.lowerLimit);
+  }
+
+  porcentuality_reduced(t, includeUpperLimit) {
+    if (n == this.lowerLimit) {
+      return 0;
+    }
+    if (n == this.upperLimit) {
+      return includeUpperLimit ? 1 : 0;
+    }
+    let p = this.porcentuality(t);
+    let Vpr = p % 1;
+    let retorno = n > 0 ? Vpr : 1 + Vpr;
+    if (includeUpperLimit && retorno == 0 && p > 0) {
+      retorno = 1;
+    }
+    return retorno;
+  }
+
+  inverse(t) {
+    return this.upperLimit + this.lowerLimit - t;
+  }
+
+  discretize(t, levels) {
+    if (levels > 1) {
+      if (levels == 2) {
+        if (t < (this.upperLimit + this.lowerLimit) / 2) {
+          return this.lowerLimit;
+        } else {
+          return this.upperLimit;
+        }
+      }
+      let Δlevels = levels - 1;
+      let value_porcent = this.porcentuality(t);
+      let v = Math.round(Δlevels * value_porcent) / Δlevels;
+      return this.lerp(v);
+    }
+    return this.upperLimit;
+  }
+
+  setCircular(n) {
+    if (this.valoration(n) == POR_DENTRO) {
+      return n;
+    }
+    return this.porcentuality_reduced(n) * this.mag + this.upperLimit;
+  }
+
+  setReflex(n) {
+    let Vp = this.porcentuality(n);
+    if (Math.floor(Vp) % 2 == 0 || this.porcentuality_reduced(n) == 0) {
+      return this.setCircular(n);
+    } else {
+      return this.inverse(this.setCircular(n));
+    }
   }
 }
